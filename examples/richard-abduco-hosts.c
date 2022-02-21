bce420cb96fac5ebaf56f5bac372a79cc75932e8
rick <rick@ricks-MacBook-Air.local>
2022-02-20 21:04:55 -0500

    m

--- examples/richard-abduco-hosts.c
+++ examples/richard-abduco-hosts.c
@@ -3,10 +3,28 @@
 
 typedef struct {
   char dummy;
-} w1, w2;
+} w1, w2, w3;
 
 
 /*  HOST FXNS */
+const bool w_ok(void) {
+  return(false);
+}
+
+
+const bool w1_ok(void) {
+  return(w_ok());
+}
+
+
+const bool w2_ok(void) {
+  return(w_ok());
+}
+
+
+const bool w3_ok(void) {
+  return(true);
+}
 
 
 const int w1_port(void) {
@@ -24,16 +42,44 @@ const int w2_port(void) {
 }
 
 
+const int w3_port(void) {
+  return(SSH_PORT);
+}
+
+
+const char *w3_name(void) {
+  return("al1");
+}
+
+
 const char *w2_name(void) {
   return("web1.vpntech.net");
 }
 
 
-void w2_connect(AbducoHost droid) {
-  AbducoHost_connect(droid);
-  puts("Waaaaoow!");
+void w1_connect(AbducoHost ah) {
+  log_debug("pre-connect>w1 custom connect!");
+  AbducoHost_connect(ah);
+  log_debug("post-connect>w1 custom connect!");
 }
 
+
+void w2_connect(AbducoHost ah) {
+  log_debug("pre-connect>w2 custom connect!");
+  AbducoHost_connect(ah);
+  log_debug("post-connect>w2 custom connect!");
+}
+
+
+void w3_connect(AbducoHost ah) {
+  log_debug("pre-connect>w3 custom connect!");
+  AbducoHost_connect(ah);
+  log_debug("post-connect>w3 custom connect!");
+}
+
+#define w1_connect_CUSTOM    ()
 impl(AbducoHost, w1);
 #define w2_connect_CUSTOM    ()
 impl(AbducoHost, w2);
+#define w3_connect_CUSTOM    ()
+impl(AbducoHost, w3);
--- examples/richard-dev.c
+++ examples/richard-dev.c
@@ -1,14 +1,23 @@
 
-#define AbducoHost_IFACE          \
-  vfunc(const char *, name, void) \
-  vfunc(const int, port, void)    \
-  vfuncDefault(void, connect, AbducoHost droid)
+#define AbducoHost_IFACE                                 \
+  vfunc(const char *, name, void)                        \
+  vfunc(const int, port, void)                           \
+  vfunc(const bool, ok, AbducoHost ah)                   \
+  vfuncDefault(void, connect, AbducoHost ah)             \
+  vfuncDefault(const char *, sessions, AbducoHost ah)
 
 interface(AbducoHost);
 
+bool AbducoHost_ok(AbducoHost ah) {
+  log_debug("checking ok on %s", ah.vptr->name());
+  return true;
+}
+char * AbducoHost_sessions(AbducoHost ah) {
+  log_debug("listing sessions on %s", ah.vptr->name());
+}
 
-void AbducoHost_connect(AbducoHost droid) {
-  log_debug("Connecting to %s on port %d...", droid.vptr->name(), droid.vptr->port());
+void AbducoHost_connect(AbducoHost ah) {
+  log_debug("Connecting to %s on port %d...", ah.vptr->name(), ah.vptr->port());
 }
 
 #include "richard-abduco-hosts.c"
@@ -16,12 +25,13 @@ void AbducoHost_connect(AbducoHost droid) {
 
 int dev2(){
   AbducoHost h1 = DYN(w1, AbducoHost, &(w1){ 0 });
-
   VCALL_OBJ(h1, connect);
-
+  /*
   AbducoHost h2 = DYN(w2, AbducoHost, &(w2){ 0 });
-
   VCALL_OBJ(h2, connect);
+  AbducoHost h3 = DYN(w3, AbducoHost, &(w3){ 0 });
+  VCALL_OBJ(h3, connect);*/
+
   return(0);
 }
 
